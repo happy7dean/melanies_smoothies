89a0b8d0d5b85baaaf6496cf760469d3275b1cac
happy7dean <happy7dean@gmail.com>
2025-05-09 17:42:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,9 @@ connection_parameters = {
 # Create Snowpark session
 session = Session.builder.configs(connection_parameters).create()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
